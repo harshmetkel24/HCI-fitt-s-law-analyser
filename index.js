41754955e5b4e29160408d9f41d47092c0e515bf
Harshkumar Metkel <harshmetkel24@gmail.com>
2023-06-12 23:49:50 +0530

    pdf report is downloading

--- index.js
+++ index.js
@@ -1,37 +1,4 @@
-function calculateFittsLaw(target, startTime) {
-  const targetWidth = target.offsetWidth;
-  const targetDistance = target.getBoundingClientRect().left;
-  const index = Math.log2(targetDistance / targetWidth + 1);
-  const endTime = performance.now() - startTime;
-
-  return { targetWidth, targetDistance, endTime, index };
-}
-
-// Perform Fitts' Law analysis and generate report
-function performAnalysis(event) {
-  const target = event.target;
-  const startTime = performance.now();
-  const analysis = calculateFittsLaw(target, startTime);
-
-  console.log("Target Width:", analysis.targetWidth);
-  console.log("Target Distance:", analysis.targetDistance);
-  console.log("Time:", analysis.endTime);
-  console.log("Index of Difficulty:", analysis.index);
-
-  // Generate report
-  const report = `
-        Fitts' Law Analysis Report:
-        ----------------------------
-        Target Width: ${analysis.targetWidth}
-        Target Distance: ${analysis.targetDistance}
-        Time: ${analysis.endTime.toFixed(2)} milliseconds
-        Index of Difficulty: ${analysis.index}
-      `;
-
-  console.log(report);
-}
-
-// document.getElementById("target").addEventListener("click", performAnalysis);
+let reportData = ``;
 
 const renderHtml = function (event) {
   document.querySelector(".output-container").classList.remove("d-none");
@@ -41,8 +8,77 @@ const renderHtml = function (event) {
   const renderedHtml = document.querySelector(".rendered-html");
 
   renderedHtml.innerHTML = html;
-  console.log(css);
   document.querySelector(".rendered-html").innerHTML += `<style>${css}</style>`;
+
+  // Attach event listeners to target elements
+  for (let i = 0; i < inputTargetElements.length; i++) {
+    inputTargetElements[i].addEventListener("mouseenter", handleInteraction);
+  }
 };
 
+const downloadReport = function () {
+  var val = htmlToPdfmake(reportData);
+  var dd = { content: val };
+  console.log(reportData);
+  pdfMake.createPdf(dd).download();
+};
+
+document
+  .querySelector("#download-btn")
+  .addEventListener("click", downloadReport);
+
 document.querySelector(".render-btn").addEventListener("click", renderHtml);
+
+const inputTargetElements = document.getElementsByTagName("input");
+
+const performAnalysis = function (event) {
+  console.log(inputTargetElements);
+};
+
+const getTargetDimensions = (target) => {
+  const { width, height } = target.getBoundingClientRect();
+  return { width, height };
+};
+
+const handleInteraction = (event) => {
+  const target = event.target;
+  const startTime = performance.now();
+
+  // Step 4: Measure the user's movement time
+  const handleInteractionEnd = () => {
+    const endTime = performance.now();
+    const movementTime = endTime - startTime;
+
+    // Step 5: Calculate Fitts' Law parameters
+    const { width, height } = getTargetDimensions(target);
+    const distance = calculateDistanceToTarget(target);
+    const index = Math.log2(distance / width + 1);
+
+    // Step 6: Analyze the data (you can perform calculations or store data as needed)
+
+    // Step 7: Generate the report (you can log the results or display them in a report format)
+    reportData += `<h3>Target Width: ${width}</h3>
+    <h3>Target Distance: ${distance}</h3>
+    <h3>Movement Time: ${movementTime.toFixed(2)} milliseconds</h3>
+    <h3>Index of Difficulty: ${index}</h3>
+    <hr>
+    `;
+    console.log("Target Width:", width);
+    console.log("Target Distance:", distance);
+    console.log("Movement Time:", movementTime.toFixed(2), "milliseconds");
+    console.log("Index of Difficulty:", index);
+  };
+
+  target.addEventListener("mouseleave", handleInteractionEnd);
+  target.addEventListener("mouseup", handleInteractionEnd);
+};
+
+// Helper function to calculate the distance from the cursor to the target
+const calculateDistanceToTarget = (target) => {
+  const targetRect = target.getBoundingClientRect();
+  const targetX = targetRect.left + targetRect.width / 2;
+  const targetY = targetRect.top + targetRect.height / 2;
+  const cursorX = event.clientX;
+  const cursorY = event.clientY;
+  return Math.sqrt((targetX - cursorX) ** 2 + (targetY - cursorY) ** 2);
+};
